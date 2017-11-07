6bb4e384f5e6a70a32245077aa497785e9f556d6
Prasad Mangle <prasadmangle@gmail.com>
2017-11-07 11:15:21 +0530

    updted exams layyout added list of questions

--- server/models/exam.ts
+++ server/models/exam.ts
@@ -1,8 +1,14 @@
 import * as mongoose from 'mongoose';
 
+const questionSchema = new mongoose.Schema({
+  question: String,
+  correctAnswer: String
+});
+
 const examSchema = new mongoose.Schema({
   name: String,
-  topic: String
+  topic: String,
+  questions : [questionSchema]
 });
 
 const Exam = mongoose.model('Exam', examSchema);
